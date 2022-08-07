560608b1ca07dee45483f0378fa8fadbef645c23
Aukik Aurnab <55758994+aukik@users.noreply.github.com>
2022-08-07 15:40:03 +0600

    new

--- src/main/java/com/controller/AddConfig.java
+++ src/main/java/com/controller/AddConfig.java
@@ -1,9 +1,18 @@
 package com.controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan("com.controller")
 public class AddConfig {
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver vr=new InternalResourceViewResolver();
+        vr.setSuffix("/webapp/");
+        vr.setSuffix(".jsp");
+        return vr;
+    }
 }
